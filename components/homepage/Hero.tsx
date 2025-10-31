cbbebecf810394e0a7ac5f2906d5c00119e2bb9d
Amosram <amosrama733@gmail.com>
2025-10-31 15:23:51 +0300

    new pages

--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -151,7 +151,7 @@ export default function Hero() {
               </div>
             </Link>
             <Link
-              href="/contact"
+              href="/contact-us"
               className="bg-gradient-to-br from-[#5C1A25] via-[#7A2C38] to-[#A84B58] w-fit border border-white/60
               pl-5 pr-3 py-2 rounded-full flex items-center gap-3 text-white 
               hover:opacity-90 transition-all duration-300 ease-in-out group"
@@ -192,7 +192,7 @@ export default function Hero() {
             className="p-2 bg-white/20 hover:bg-white text-white hover:text-black rounded-full shadow-md transition-all cursor-pointer"
           >
             <ChevronRight className="w-5 h-5" />
-          </button>
+          </button>              
         </div>
       </section>
     </main>
