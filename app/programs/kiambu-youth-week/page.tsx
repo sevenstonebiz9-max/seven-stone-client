f5c074f66a77e8e0aeebedde2cfd0c5071e5b210
Amosram <amosrama733@gmail.com>
2025-11-10 15:39:35 +0300

    image optimization

--- app/programs/kiambu-youth-week/page.tsx
+++ app/programs/kiambu-youth-week/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
+    <main>
       <KiambuYouthWeek/>
-    </div>
+    </main>
   )
 }
 
